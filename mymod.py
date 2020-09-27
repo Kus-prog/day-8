c700a04fc986ef3d828843266c7a17e23c9476e9
Kus-prog <70935502+Kus-prog@users.noreply.github.com>
2020-09-27 21:25:41 +0530

    Add files via upload

---
+++ mymod.py
@@ -0,0 +1,16 @@
+import hello
+result=hello.greeting('kusum')
+print(result)
+a=hello.person1["age"]
+print(a)
+
+import hello as m
+b=m.person1["age"]
+print(b)
+c=m.person1["country"]
+print(c)
+d=m.person1["name"]
+print(d)
+
+
+
